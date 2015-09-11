4cbe2225e1396b95c1d4b2027cffc915283910cc
Jeff Simpson <jeff@syncrodoka.net>
2015-09-11 09:52:43 -0600

    Updated nix shell env

--- scudcloud-1.0/default.nix
+++ scudcloud-1.0/default.nix
@@ -1,6 +1,6 @@
 with import <nixpkgs> {}; {
   pyEnv = stdenv.mkDerivation {
     name = "py";
-    buildInputs = [ stdenv python3 python3Packages.virtualenv python3Packages.pyqt5 sip ];
+    buildInputs = [ stdenv python3 python3Packages.virtualenv python3Packages.pyqt5 python3Packages.sip_4_16 qt5.multimedia ];
   };
 }
