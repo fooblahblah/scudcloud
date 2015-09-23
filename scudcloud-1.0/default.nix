39885fb06d042b908503826ed94f4e68a90075ae
Jeff Simpson <jeff@syncrodoka.net>
2015-09-23 16:41:58 -0600

    WIP

--- scudcloud-1.0/default.nix
+++ scudcloud-1.0/default.nix
@@ -1,6 +1,6 @@
 with import <nixpkgs> {}; {
   pyEnv = stdenv.mkDerivation {
     name = "py";
-    buildInputs = [ stdenv python3 python3Packages.virtualenv python3Packages.pyqt5 python3Packages.sip_4_16 qt5.multimedia ];
+    buildInputs = [ stdenv python3 python3Packages.virtualenv python3Packages.pyqt5 python3Packages.sip_4_16 qt5.multimedia phonon_qt5 libpulseaudio phonon_qt5_backend_gstreamer gstreamer gst_plugins_base gst_plugins_good gst_plugins_bad ];
   };
 }
