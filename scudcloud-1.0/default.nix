3c590ce840194cb6f02a7961083fe62ce0063d45
Jeff Simpson <jeff@syncrodoka.net>
2016-03-08 08:41:19 -0700

    Updated with latest pyqt5

--- scudcloud-1.0/default.nix
+++ scudcloud-1.0/default.nix
@@ -1,6 +1,7 @@
 with import <nixpkgs> {}; {
   pyEnv = stdenv.mkDerivation {
     name = "py";
-    buildInputs = [ stdenv python3 python3Packages.virtualenv python3Packages.pyqt5 python3Packages.sip_4_16 qt5.multimedia phonon_qt5 libpulseaudio phonon_qt5_backend_gstreamer gstreamer gst_plugins_base gst_plugins_good gst_plugins_bad ];
+    buildInputs = [ stdenv python3 python3Packages.virtualenv python3Packages.pyqt5 python3Packages.sip_4_16 qt5.qtmultimedia phonon_qt5 libpulseaudio phonon_qt5_backend_gstreamer gstreamer gst_plugins_base gst_plugins_good gst_plugins_bad glib_networking libsoup cacert ];
+    LD_LIBRARY_PATH="${glib_networking}/lib/gio/modules:${libsoup}/lib";
   };
 }
